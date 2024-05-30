899bcb997e52dc483362a9804c4848272e6d115a
23 1 <liaw911114@gmail.com>
2024-05-30 22:27:42 +0800

    Update json format

--- tools/NYCUTimeTableCrawler.py
+++ tools/NYCUTimeTableCrawler.py
@@ -4,14 +4,15 @@ import aiohttp
 
 class NYCUTimeTableCrawler:
 
-    def __init__(self, acy:int, sem:int, acyend:int, semend:int):
+    def __init__(self, acy:int, sem:int):
         self.timeTableUrl = "https://timetable.nycu.edu.tw/"
         self.acy = str(acy)
         self.sem = str(sem)
-        self.acyend = str(acyend)
-        self.semend = (semend)
+        self.acyend = str(acy)
+        self.semend = (sem)
         self.acysem = f"{self.acy}{self.sem}"
         self.acysemend = f"{self.acyend}{self.semend}"
+        self.allDepartments = []
         self.paramDefault = {
             "flang": "zh-tw",
             "acysem": self.acysem, 
@@ -20,13 +21,6 @@ class NYCUTimeTableCrawler:
             "fcategory": "*",
             "fcollege": "*",
             }
-
-
-    def saveDepartmentIdAndPath(self, savePath):
-        self.allParams = []
-        self.getType()
-        with open(savePath, "w") as f:
-            json.dump(self.allParams, f, ensure_ascii=False, indent=4)
 
     # A function that sent http post request given the url, header and  data-form
     def sent(self, param, data):
@@ -58,41 +52,74 @@ class NYCUTimeTableCrawler:
         formData["m_selcampus"] = "**"
         return self.sent({'r': 'main/get_cos_list'}, formData).json()
     
-    def getType(self):
+    def getOutline(self, courseId):
+        formData = {}
+        formData["acy"] = self.acy
+        formData["sem"] = self.sem
+        formData["cos_id"] = courseId
+        return self.sent({'r': 'main/getCrsOutlineDescription'}, formData).json()
+
+    def getDepartmentIdAndPath(self):
+        self.dept_params_path = []
+        self._getType() # Get all the parameters to sent http request and store in self.dept_params_path
+        self.allDepartmentsResult = [[] for _ in range(len(self.dept_params_path))] # The list to store response from multi threading http request
+
+        def getDeptId(idx):
+            params, path = self.dept_params_path[idx]
+            departments = self.sent({'r': 'main/get_dep'}, params).json()
+            # print(f"dapartment: {departments}")
+            for departmentId, departmentName in departments.items():
+                departmentPath = f"{path}_{departmentName}"
+                self.allDepartmentsResult[idx].append({"departmentPath": departmentPath, "departmentId": departmentId})
+
+        import threading
+        from tqdm import tqdm
+        threads = []
+        for i in range(0, len(self.dept_params_path)):
+            thread = threading.Thread(target=getDeptId, args=(i,))
+            thread.start()
+            threads.append(thread)
+        # Wait for all threads to finish
+        for thread in tqdm(threads):
+            thread.join()
+        self.allDepartments = [r for dept in self.allDepartmentsResult for r in dept]
+        return self.allDepartments
+
+        # savePath = f"{self.acysem}_departments.json"
+        # with open(savePath, "w", encoding="utf8") as f:
+        #     json.dump(self.allDepartments, f, ensure_ascii=False, indent=4)
+    
+    def _getType(self):
         types = self.sent({'r': 'main/get_type'}, self.paramDefault).json()
         for type in types:
             depId = type['uid']
             depName = type['cname']
             paramGetCategory = self.paramDefault.copy()
             paramGetCategory["ftype"] = depId
-            self.getCategory(paramGetCategory, f"{depName}")
+            self._getCategory(paramGetCategory, f"{depName}")
 
 
-    def getCategory(self, params, path:str):
+    def _getCategory(self, params, path:str):
         categories =  self.sent({'r': 'main/get_category'}, params).json()
         for categoryId, categoryName in categories.items():
             paramGetCollege = params.copy()
             paramGetCollege["fcategory"] = categoryId
             # print(f"category: {categoryId} {categoryName}")
             if params["ftype"] in ["870A5373-5B3A-415A-AF8F-BB01B733444F", "D8E6F0E8-126D-4C2F-A0AC-F9A96A5F6D5D"]:
-                self.getCollege(paramGetCollege, f"{path}_{categoryName}")
+                self._getCollege(paramGetCollege, f"{path}_{categoryName}")
             else:
-                self.getDepartment(paramGetCollege, f"{path}{'_'+categoryName if categoryName else ''}")
+                self._getDepartment(paramGetCollege, f"{path}{'_'+categoryName if categoryName else ''}")
 
-        
-
-    def getCollege(self, params, path:str):
+    def _getCollege(self, params, path:str):
         colleges =  self.sent({'r': 'main/get_college'}, params).json()
         for collegeId, collegeName in colleges.items():
             # print(f"college: {collegeId} {collegeName}")
             paramGetDepartment = params.copy()
             paramGetDepartment["fcollege"] = collegeId
-            self.getDepartment(paramGetDepartment, f"{path}{'_'+collegeName if collegeName else ''}")
+            self._getDepartment(paramGetDepartment, f"{path}{'_'+collegeName if collegeName else ''}")
 
-    def getDepartment(self, params, path:str):
-        departments = self.sent({'r': 'main/get_dep'}, params).json()
-        # print(f"dapartment: {departments}")
-        for departmentId, departmentName in departments.items():
-            departmentPath = f"{path}_{departmentName}"
-            print(departmentPath)
-            self.allParams.append({"departmentPath": departmentPath, "departmentId": departmentId})
+    def _getDepartment(self, params, path:str):
+        """
+        Used for gathering http request parameters
+        """
+        self.dept_params_path.append([params, path])
--- tools/retrieveCourse.py
+++ tools/retrieveCourse.py
@@ -6,7 +6,7 @@ import json
 
 timeTableUrl = "https://timetable.nycu.edu.tw/"
 
-nycuTimeTableCrawler = NYCUTimeTableCrawler(113, 1, 113, 1)
+nycuTimeTableCrawler = NYCUTimeTableCrawler(113, 1)
 
 def extraceCourseInfo(courseDetail, coursePath:str):
     courseInfoList = []
@@ -39,9 +39,7 @@ def extraceCourseInfo(courseDetail, coursePath:str):
                     courseInfo["costype"].append(type)
             # get language
             courseInfo["language"] = courseDetail["language"][courseId]["授課語言代碼"]
-
             courseInfoList.append(courseInfo)
-
     return courseInfoList
 
 
@@ -49,27 +47,27 @@ courseCount = 0
 # logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.DEBUG)
 logging.basicConfig(handlers=[logging.FileHandler('example.log', 'w', 'utf-8')], level=logging.DEBUG)
 
-with open("departmentPath.json", "r", encoding="utf8") as f:
-    courseParams = json.load(f)
+print(f"Get department Id and Path")
+courseParams = nycuTimeTableCrawler.getDepartmentIdAndPath()
 
-result = []
 courseDetails = {k:None for k in range(0, len(courseParams))}
 def getCourseDetail(courseParams, i):
-    coursePath = courseParams[i]["departmentPath"]
     departmentId = courseParams[i]["departmentId"] 
-    print(f"Find course {i} under {coursePath}, currently have {courseCount} courses")
     depCourseDetail = nycuTimeTableCrawler.getCourseList(departmentId)
     courseDetails[i] = depCourseDetail
 
 threads = []
+print(f"Create http request for course detail.")
 for i in tqdm(range(0, len(courseParams))):
     thread = threading.Thread(target=getCourseDetail, args=(courseParams, i))
     thread.start()
     threads.append(thread)
 # Wait for all threads to finish
+print(f"Waiting for http response for course detail.")
 for thread in tqdm(threads):
     thread.join()
 
+result = []
 for idx, depCourseDetail in courseDetails.items():
     coursePath = courseParams[idx]["departmentPath"]
     if len(depCourseDetail) == 0:
@@ -79,9 +77,40 @@ for idx, depCourseDetail in courseDetails.items():
         # print(courseDetail['dep_cname'])
         courseInfoList = extraceCourseInfo(courseDetail, coursePath)
         result += courseInfoList
-        courseCount = len(result)
+print(f"Total course count: {len(result)}")
+# Dedupe
+unique_course = {}
+for course in result:
+    cos_id = course['cos_id']
+    if cos_id in unique_course.keys():
+        if isinstance(unique_course[cos_id]['coursePath'], str):
+           unique_course[cos_id]['coursePath'] = [unique_course[cos_id]['coursePath'], course['coursePath']]
+        else:
+            unique_course[cos_id]['coursePath'].append(course['coursePath'])
+    else:
+        unique_course[cos_id] = course
+        unique_course[cos_id]['coursePath'] = [unique_course[cos_id]['coursePath'], course['coursePath']]
+result = list(unique_course.values())
+print(f"Total course count after dedup: {len(result)}")
 
-print(f"Total course count: {courseCount}")
-file_name = f"{nycuTimeTableCrawler.acysem}_{nycuTimeTableCrawler.acysemend}.json"
+# Retrieve course outline
+thread_num = 512
+for i in tqdm(range(0, len(result), thread_num)):
+    def getCourseOutline(key):
+        global result
+        courseId = result[key]['cos_id']
+        response = nycuTimeTableCrawler.getOutline(courseId)
+        outline = "" if not response else response["crs_outline"]
+        result[key]['crs_outline'] = outline
+    threads = []
+    end_idx = min(thread_num, len(result) - i)
+    for j in range(0, end_idx):
+        thread = threading.Thread(target=getCourseOutline, args=(i + j,))
+        thread.start()
+        threads.append(thread)
+    for thread in threads:
+        thread.join()
+
+file_name = f"{nycuTimeTableCrawler.acysem}.json"
 with open(file_name, "w", encoding="utf8") as f:
     json.dump(result, f, ensure_ascii=False, indent=4)
